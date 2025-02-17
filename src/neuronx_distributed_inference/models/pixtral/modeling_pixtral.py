e8706ab08c99ab797344fc5a262cb80b119a5ea1
EmilyWebber <egwebber@amazon.com>
2025-02-17 19:41:42 +0000

    Ready to debug forward pass

--- src/neuronx_distributed_inference/models/pixtral/modeling_pixtral.py
+++ src/neuronx_distributed_inference/models/pixtral/modeling_pixtral.py
@@ -394,15 +394,14 @@ class NeuronPixtralModel(NeuronBaseModel):
                     ])
             return inputs_embeds
     
-        def forward(
-            self,
-            input_ids: torch.Tensor,
-            positions: torch.Tensor,
-            kv_caches: List[torch.Tensor],
-            attn_metadata: AttentionMetadata,
-            intermediate_tensors: Optional[IntermediateTensors] = None,
-            inputs_embeds: Optional[torch.Tensor] = None,
-            **kwargs: object,
+        def forward(self,
+                    input_ids: torch.Tensor,
+                    positions: torch.Tensor,
+                    kv_caches: List[torch.Tensor],
+                    attn_metadata: AttentionMetadata,
+                    intermediate_tensors: Optional[IntermediateTensors] = None,
+                    inputs_embeds: Optional[torch.Tensor] = None,
+                    **kwargs: object,
         ) -> Union[torch.Tensor, IntermediateTensors]:
             """Run forward pass for pixtral.
             """
@@ -417,12 +416,12 @@ class NeuronPixtralModel(NeuronBaseModel):
                                                           vision_embeddings)
                 input_ids = None
     
-            hidden_states = self.language_model.model(input_ids,
-                                                      positions,
-                                                      kv_caches,
-                                                      attn_metadata,
-                                                      intermediate_tensors,
-                                                      inputs_embeds=inputs_embeds)
+            hidden_states = self.text_model.forward(input_ids,
+                                                  positions,
+                                                  kv_caches,
+                                                  attn_metadata,
+                                                  intermediate_tensors,
+                                                  inputs_embeds=inputs_embeds)
     
             return hidden_states
     
